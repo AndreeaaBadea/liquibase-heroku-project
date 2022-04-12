226ab1669ade44821fb3b04307d5df342faf9506
AndreeaaBadea <andreea.badea@capgemini.com>
2022-04-12 20:18:08 +0300

    Modify enity class

--- src/main/java/com/example/liquidbaseprojectAB/entity/Person.java
+++ src/main/java/com/example/liquidbaseprojectAB/entity/Person.java
@@ -4,15 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "persons")
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
 
+    @Column(name = "firstName")
     private String firstName;
 
+    @Column(name = "lastName")
     private String lastName;
 
 }
