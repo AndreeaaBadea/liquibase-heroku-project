74dea457d321a7a24131939131b7d20c325b8ff8
AndreeaaBadea <andreea.badea@capgemini.com>
2022-04-21 13:54:29 +0300

    Added exception handler

--- src/main/java/com/example/liquidbaseprojectAB/controller/UserController.java
+++ src/main/java/com/example/liquidbaseprojectAB/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.example.liquidbaseprojectAB.controller;
 
 import com.example.liquidbaseprojectAB.entity.dto.UserDto;
+import com.example.liquidbaseprojectAB.exception.NoSuchUserException;
+import com.example.liquidbaseprojectAB.exception.ResponseMessage;
 import com.example.liquidbaseprojectAB.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,7 +36,18 @@ public class UserController {
 
     @DeleteMapping()
     public ResponseEntity<String> deleteUser(@RequestParam int id){
-        return new ResponseEntity<>("Delete person id " + id, HttpStatus.OK);
+        return new ResponseEntity<String>("User deleted.", HttpStatus.OK);
+    }
+
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<ResponseMessage> handleJsonFormatException(Exception exception){
+        return new ResponseEntity<ResponseMessage>(new ResponseMessage("INVALID INPUT"), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NoSuchUserException.class)
+    public ResponseEntity<ResponseMessage> handleNotFound(Exception exception){
+        return new ResponseEntity<ResponseMessage>(new ResponseMessage("NO SUCH PROJECT"), HttpStatus.NOT_FOUND);
     }
 
 }
--- src/main/java/com/example/liquidbaseprojectAB/service/UserService.java
+++ src/main/java/com/example/liquidbaseprojectAB/service/UserService.java
@@ -2,6 +2,8 @@ package com.example.liquidbaseprojectAB.service;
 
 import com.example.liquidbaseprojectAB.entity.User;
 import com.example.liquidbaseprojectAB.entity.dto.UserDto;
+import com.example.liquidbaseprojectAB.exception.AccessDatabaseException;
+import com.example.liquidbaseprojectAB.exception.NoSuchUserException;
 import com.example.liquidbaseprojectAB.repository.UserRepository;
 import com.example.liquidbaseprojectAB.service.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,21 +19,34 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    public UserDto addUser(UserDto userDto){
+    public UserDto addUser(UserDto userDto) {
         User user = userRepository.save(UserMapper.toEntity(userDto));
         Logger.info("Added user id {}.", userDto.getId());
         return UserMapper.toDto(user);
     }
 
-    public List<UserDto> getAllUsers(){
+    public List<UserDto> getAllUsers() {
         return userRepository.findAll()
                 .stream()
                 .map(UserMapper::toDto)
                 .collect(Collectors.toList());
     }
 
-    public UserDto getUserById(int id){
-        return UserMapper.toDto(userRepository.getById(id));
+    public UserDto getUserById(int id) {
+        User user = userRepository.getById(id);
+        if(user == null){
+            Logger.info("There is no user with given id.");
+            throw new NoSuchUserException("There is no user with given id.");
+        }
+        return UserMapper.toDto(user);
     }
 
+    public void deleteUserById(int id) {
+        userRepository.getById(id);
+        Logger.info("Customer id {} deleted from database. ", id);
+    }
 }
+
+
+
+
